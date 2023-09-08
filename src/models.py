4e14997e6bccdc99a373a300cfb4f450ae282727
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-09-08 15:55:17 +0000

    falta añadir relaciones

--- src/models.py
+++ src/models.py
@@ -9,42 +9,36 @@ Base = declarative_base()
 
 class Follower(Base):
     __tablename__ = 'follower'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    user_from_id= Column(Integer, ForeignKey('user.id'))
+    user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    username = Column(String(250), index=true)
-    firstname = Column(String(250))
-    lastname = Column(String(250))
-    email = Column(String(100), unique=true)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(50), unique=True)
 
 class Media(Base):
     __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    type = Column(enumerate)
-    url = Column(String(250))
-    post_id = Column(Integer, ForeignKey('post.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String(50), nullable=False)
+    url = Column(String(50), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 class Comment(Base):
     __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
 
 ## Draw from SQLAlchemy base
 try:
